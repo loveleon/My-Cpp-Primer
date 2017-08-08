6f7f680b032db0ace9e1558277e2261efe007c19
331527583 <331527583@qq.com>
2017-08-09 01:28:28 +0800

    modify the bug of code

--- ch03/ex3_35-my.cpp
+++ ch03/ex3_35-my.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main() {
   int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  const int* p = a;
-  for (auto i = 0; i < 10; i++) *(p + i) = 0;
-  for (auto num : a) cout << num << " " cout << endl;
+  int* const p = a;
+  for (auto i = 0; i < 10; i++) {
+    cout << *(p + i) << " ";
+    *(p + i) = 0;
+  }
+  cout << endl;
+
+  for (auto num : a) cout << num << " ";
+  cout << endl;
   return 0;
 }
