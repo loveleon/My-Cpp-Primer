546851822196f70e715f23b1a0ef6c36b70419f8
2551247912 <2551247912@qq.com>
2017-08-24 00:13:16 +0800

    modify:init a vector with an array

--- ch03/ex3_41-my.cpp
+++ ch03/ex3_41-my.cpp
@@ -3,10 +3,18 @@
 #include <iostream>
 using namespace std;
 
+// int main() {
+//   int arr[] = {1, 2, 3, 4, 5, 6};
+//   vector<int> vec = {begin(arr), end(arr)};
+//   for (auto i = vec.begin(); i != vec.end(); i++) cout << *i << " ";
+//   cout << endl;
+//   return 0;
+// }
+
 int main() {
-  int arr[] = {1, 2, 3, 4, 5, 6};
-  vector<int> vec = {begin(arr), end(arr)};
-  for (auto i = vec.begin(); i != vec.end(); i++) cout << *i << " ";
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  vector<int> v(begin(arr), end(arr));
+  for (auto i : v) cout << i << " ";
   cout << endl;
   return 0;
 }
