d71a5632b76c9748b12ef150ffc3b8679543244e
2551247912 <2551247912@qq.com>
2017-09-05 18:42:58 +0800

    modify the error of ex9_22

--- ch09/ex9_22-my.cpp
+++ ch09/ex9_22-my.cpp
@@ -2,17 +2,16 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
-	vector<int> vec{1,2,3,4,5,6,7,8,9};
-	vector<int>::iterator it = vec.begin(),mid = vec.begin() + vec.size()/2;
-	while(it != mid)
-	{
-		if(*it == 2)
-			vec.insert(it,2*2);
-	}
-	for(auto i : vec)
-		cout << i << " ";
-	cout << endl;
-	return 0;
+int main() {
+  vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9};
+  vector<int>::iterator ittmp;
+  vector<int>::iterator it = vec.begin(), mid = vec.begin() + vec.size() / 2;
+  while (it != mid) {
+    ittmp = it;
+    if (*it == 2) vec.insert(ittmp, 2 * 2);
+    ++it;
+  }
+  for (auto i : vec) cout << i << " ";
+  cout << endl;
+  return 0;
 }
