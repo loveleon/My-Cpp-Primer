85bfe9f5e3af27d79e4c77f1d8d93a1aff540681
2551247912 <2551247912@qq.com>
2017-09-06 10:01:53 +0800

    about vector erase function
    when erase(b,e) and e is the end() ,then return the first item iterator
    .This is not normal ???

---
+++ ch09/ex9_25-my.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+  vector<int> vec{11, 22, 33, 44, 55};
+  // auto it = vec.erase(vec.begin(), vec.begin());//return the first item
+  // auto it = vec.erase(vec.begin(), vec.begin() + 2);//return the begin() + 2
+  // item
+  // auto it = vec.erase(vec.end(), vec.end());//return the first item iterator
+  auto itVecEnd = vec.begin();
+  auto it = vec.erase(vec.begin(), --(vec.end()));  // return the last item
+  auto it = vec.erase(vec.begin(),
+                      vec.end());  // return the begin/first item iterator
+  cout << *it << "|" << endl;
+  if (it == itVecEnd) cout << "return the begin iterator ,why ?" << endl;
+  for (auto itAfterDel = vec.begin(); itAfterDel != vec.end(); ++itAfterDel)
+    cout << *itAfterDel << endl;
+  return 0;
+}
