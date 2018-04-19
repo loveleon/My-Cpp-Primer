84c72918731800f18ae976fb28e7aee91ee4d630
2551247912@qq.com <2551247912@qq.com>
2018-04-19 10:12:09 +0800

    commit comm head files.

---
+++ ch06/comm/comm.h
@@ -0,0 +1,13 @@
+//
+// Created by cxh on 18-1-1.
+//
+
+#ifndef CH06_COMM_H
+#define CH06_COMM_H
+#include <string>
+#include <iostream>
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+#endif //CH06_COMM_H
