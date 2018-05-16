4e4c7290310942dc430517d92ed6526582fcfdcf
2551247912@qq.com <2551247912@qq.com>
2018-05-17 00:02:34 +0800

    ex11_7.cpp

---
+++ ch11/ex11_7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+using std::map;
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(){
+    using VEC = vector<string>;
+    using VECIT = VEC::iterator; map<string,VEC>  mapNm;
+
+}
