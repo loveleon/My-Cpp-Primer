fba94c939fa171b99976a831bda8a6a34df091d7
2551247912@qq.com <2551247912@qq.com>
2018-06-11 23:24:06 +0800

    modify ex12_32.

--- ch12/ex12_32.cpp
+++ ch12/ex12_32.cpp
@@ -19,37 +19,11 @@ int main(){
         return -1;
     }
 
-    vector<string> vFile{};
-    map<string,set<int>> mWordLine{};
-    int nLineNo = 0;
-    //int nLineNo = 1;
-    for(string line;std::getline(infile,line);++nLineNo){
-        vFile.push_back(line);
-        istringstream is(line);
-        for(string text,word;is >> text; word.clear()){
-            std::remove_copy_if(text.begin(),text.end(),std::back_inserter(word),ispunct);
-            auto &newset = mWordLine[word];
-            newset.insert(nLineNo);
-        }
-    }
-
-    cout << "lookup" << endl;
-    for(string msg;cin >> msg;cout << "going on lookup..."){
-        if(msg == string("q") || msg == string("Q")){
-            break;
-        }
-        auto it = mWordLine.find(msg);
-        if(it != mWordLine.end()){
-        auto times =  it->second.size();
-        cout << it->first << " ocur " <<times << (times > 1?" times":" time") << endl;
-        for(auto item : it->second){
-           // cout << "   (line " << ++item << ") " <<  vFile[--std::ref(item)] << endl;
-            cout << "   (line " << item+1 << ") " <<  vFile.at(item) << endl;//nLineNo start 0
-        }
-        cout << endl;
-        }else{
-            cout << "can't find " << msg << endl;
-        }
+    TextQuery q(infile);
+    string msg;
+    while(cin >> msg){
+        auto qr = q.query(msg);
+        print(std::cout, qr) << endl;
     }
     return 0;
 }
