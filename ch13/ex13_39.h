c276aec5f2118c4e44e01d93271219777c1e2732
2551247912@qq.com <2551247912@qq.com>
2018-06-27 18:28:42 +0800

    add ex13_39

--- ch13/ex13_39.h
+++ ch13/ex13_39.h
@@ -3,14 +3,29 @@
 
 class StrVec{
     public:
-        StrVec();
+        StrVec():element(nullptr),cap(nullptr),first_free(nullptr){}
         StrVec(const StrVec &v);
         StrVec &operator=(const StrVec &v);
+        ~StrVec();
+        std::string *begin(){return element;}
+        std::string *end(){return first_free;}
+        std::size_t size(){return first_free-element;}
+        std::size_t capacity(){return cap-element;}
 
-        void free();
-        void alloc_n_copy();
     private:
-        static std::allocator<std::string *> alloc;
+        //called by constructor,destructor,operator=
+        std::pair<std::string *,std::string *> alloc_n_copy(const std::string *beg,const std::string *end);
+
+        //been called by push action.
+        void chk_n_alloc();
+        //destroy item and free memory.
+        void free();
+
+        //realloc much more memory and copy
+        void reallocate();
+
+    private:
+        static std::allocator<std::string> alloc;
         std::string *element;
         std::string *cap;
         std::string *first_free;
