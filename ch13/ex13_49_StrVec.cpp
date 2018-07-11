c5b56cded11525b3ee994416d2a09e20e4c26081
2551247912@qq.com <2551247912@qq.com>
2018-07-12 00:09:17 +0800

    add ex13_49

---
+++ ch13/ex13_49_StrVec.cpp
@@ -0,0 +1,111 @@
+#include "ex13_49_StrVec.h"
+
+std::allocator<std::string> StrVec::alloc;
+StrVec::StrVec(const StrVec &v){
+   auto data = alloc_n_copy(v.begin(),v.end());
+   element = data.first;
+   first_free = cap = data.second;
+}
+
+StrVec & StrVec::operator=(const StrVec &v){
+    auto data = alloc_n_copy(v.element,v.first_free);
+    free();
+    element = data.first;
+    first_free = cap = data.second;
+    return *this;
+}
+
+StrVec::StrVec(StrVec &&s) noexcept :element(s.element),cap(s.cap),first_free(s.first_free){
+    s.element = s.first_free = s.cap = nullptr;
+}
+
+StrVec &StrVec::operator=(StrVec &&s) noexcept {
+    if(this != &s){
+        free();
+        element = s.element;
+        first_free = s.first_free;
+        cap = s.cap;
+        s.element = s.first_free = s.cap = nullptr;
+    }
+    return *this;
+}
+
+std::pair<std::string *,std::string *> StrVec::alloc_n_copy(const std::string *beg,const std::string *end){
+   auto data = alloc.allocate(end - beg);
+   return {data,std::uninitialized_copy(beg,end,data)};
+}
+
+
+void StrVec::chk_n_alloc(){
+    if(size() == capacity()){
+        reallocate();
+    }
+}
+
+void StrVec::reallocate(){
+    //
+    auto new_cap = size()?2*size():1;
+    alloc_n_move(new_cap);
+}
+
+void StrVec::alloc_n_move(std::size_t newcap){
+    auto data = alloc.allocate(cap-element);
+    auto dest = data;
+    auto elem = element;
+    for(size_t i = 0; i != size();++i){
+       alloc.construct(dest++,std::move(*elem++));
+    }
+    free();
+    element = data;
+    first_free = dest;
+    cap = element + newcap;
+}
+
+void StrVec::free(){
+    if(element){
+        for(auto p = first_free; p != element; )
+            alloc.destroy(--p);
+        alloc.deallocate(element,cap-element);
+    }
+}
+
+StrVec::~StrVec(){
+    free();
+}
+
+void StrVec::push_back(const std::string &s){
+    chk_n_alloc();
+    alloc.construct(first_free++,s);
+}
+
+void StrVec::reserve(std::size_t newcap){
+    if(newcap < capacity())
+        return ;
+    alloc_n_move(newcap);
+}
+
+void StrVec::resize(std::size_t newsize) {
+    resize(newsize,std::string());
+}
+
+void StrVec::resize(std::size_t newsize, const std::string &s){
+    if(newsize > size()){
+        if(newsize > capacity()){
+            reserve(newsize*2);
+            for(auto i = size(); i != newsize; ++i){
+                alloc.construct(first_free++,s);
+            }
+        }
+    }else if(newsize < size()){
+        for(auto it = first_free; it != element+newsize;)
+            alloc.destroy(--it);
+    }
+}
+
+int main(){
+    StrVec sv;
+    std::cout << sv.size() << std::endl;
+    sv.push_back("abc");
+    std::cout << sv.size() << std::endl;
+    return 0;
+}
---
+++ ch13/ex13_49_StrVec.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+#include <memory>
+#include <iostream>
+
+class StrVec{
+    public:
+        void reserve(std::size_t newcap);
+        void resize(std::size_t newsize);
+        void resize(std::size_t newsize, const std::string &s);
+        void push_back(const std::string &s);
+        StrVec():element(nullptr),cap(nullptr),first_free(nullptr){}
+        StrVec(const StrVec &v);
+        StrVec &operator=(const StrVec &v);
+        StrVec(StrVec &&s) noexcept;
+        StrVec &operator=(StrVec &&s) noexcept;
+        ~StrVec();
+        std::string *begin()const{return element;}
+        std::string *end()const{return first_free;}
+        std::size_t size(){return first_free-element;}
+        std::size_t capacity(){return cap-element;}
+
+    private:
+        //called by constructor,destructor,operator=
+        std::pair<std::string *,std::string *> alloc_n_copy(const std::string *beg,const std::string *end);
+
+        //been called by push action.
+        void chk_n_alloc();
+        //destroy item and free memory.
+        void free();
+
+        //realloc much more memory and copy
+        void reallocate();
+        void alloc_n_move(std::size_t newcap);
+
+    private:
+        static std::allocator<std::string> alloc;
+        std::string *element;
+        std::string *cap;
+        std::string *first_free;
+};
