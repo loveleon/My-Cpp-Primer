4181a1844b4f1a23cec01d4834dfa28d0020bde4
2551247912@qq.com <2551247912@qq.com>
2018-07-19 00:03:06 +0800

    add ex14_5

--- ch14/ex14_5.cpp
+++ ch14/ex14_5.cpp
@@ -1,4 +1,26 @@
 //
 // Created by cxh on 18-7-17.
 //
+#include "ex14_5.h"
+Book::Book(std::istream &is){
+    is >> *this;
+}
+
+std::ostream& operator<<(std::ostream& out,const Book & book){
+    out << book.no_ << " " << book.name_<< " " << book.autor_ << " " << book.pubdate_;
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Book &book){
+    in >> book.no_ >> book.name_  >> book.autor_ >> book.pubdate_;
+    return in;
+}
+
+bool operator==(const Book &lft, const Book &rhs){
+    return lft.no_ ==  rhs.no_ && lft.name_ == rhs.name_ && lft.autor_ == rhs.autor_ && lft.pubdate_ == rhs.pubdate_;
+}
+
+bool operator!=(const Book &lft, const Book &rhs){
+    return !(lft == rhs);
+}
 
--- ch14/ex14_5.h
+++ ch14/ex14_5.h
@@ -5,17 +5,26 @@
 #ifndef MY_CPP_PRIMER_EX14_5_H
 #define MY_CPP_PRIMER_EX14_5_H
 #include <string>
+#include <iostream>
 
 class Book{
     public:
         friend std::ostream& operator<<(std::ostream& out,const Book & book);
         friend std::istream& operator>>(std::istream& in, Book &book);
-
+        friend bool operator==(const Book &lft, const Book &rhs);
+        friend bool operator!=(const Book &lft, const Book &rhs);
     public:
-        Book(std::string name,std::string autor, std::string pubdate):name_(name),autor_(autor),pubdate_(pubdate){}
+        Book() = default;
+        Book(unsigned no,std::string name,std::string autor, std::string pubdate):no_(no),name_(name),autor_(autor),pubdate_(pubdate){}
+        Book(std::istream &is);
 private:
+    unsigned no_;
     std::string name_;
     std::string autor_;
     std::string pubdate_;
 };
+    std::ostream& operator<<(std::ostream& out,const Book & book);
+    std::istream& operator>>(std::istream& in, Book &book);
+    bool operator==(const Book &lft, const Book &rhs);
+    bool operator!=(const Book &lft, const Book &rhs);
 #endif //MY_CPP_PRIMER_EX14_5_H
