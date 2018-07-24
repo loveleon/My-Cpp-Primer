1a588e221ba599c3d8bdb29d69762fbc9cf11ca9
2551247912@qq.com <2551247912@qq.com>
2018-07-24 18:20:26 +0800

    modify ex14_26

--- ch14/ex14_26_String.h
+++ ch14/ex14_26_String.h
@@ -12,10 +12,26 @@ class String{
     friend bool operator>(const String &lhs, const String &rhs);
     friend bool operator>=(const String &lhs, const String &rhs);
     public:
+        String():String(""){}
         String(const char *s);
+        String& operator=(String &rhs);
+        String(const String &rhs);
+        String(String &&rhs)noexcept;
+        String& operator=(String &&rhs)noexcept;
+        ~String();
+    public:
+        void push_back(const char c);
+        char *begin(){return elements;}
+        char *end(){return last_elem;}
+        char& operator[](size_t i){return elements[i];}
+        const char& operator[](size_t i)const{return elements[i];}
     private:
         std::pair<char *,char *> alloc_n_copy(const char *beg,const char *end);
-
+        void range_initializer(const char *beg,const char *end);
+        void free();
+        void reallocate();
+        void alloc_n_move(size_t new_cap);
+        void chk_n_alloc(){if(first_free == cap) reallocate();}
 
     private:
         char * elements;
