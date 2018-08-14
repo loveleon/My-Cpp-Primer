a2e655ce960b0247888939855eec3c282fb084e3
2551247912@qq.com <2551247912@qq.com>
2018-08-14 10:34:20 +0800

    add ex15_29

--- ch15/ex15_28_29/bulk_quote.cpp
+++ ch15/ex15_28_29/bulk_quote.cpp
@@ -1,6 +1,6 @@
 #include "bulk_quote.h"
 double Bulk_quote::net_price(std::size_t n)const {
-    return n * price * ( n > quantity? 1 - discount : 1 );
+    return n * price * ( n >= quantity? 1 - discount : 1 );
 }
 
 void Bulk_quote::debug()const {
--- ch15/ex15_28_29/main.cpp
+++ ch15/ex15_28_29/main.cpp
@@ -3,6 +3,8 @@
 #include "bulk_quote.h"
 #include "disc_quote.h"
 #include <vector>
+#include <memory>
+using std::shared_ptr;
 
 void print_debug(Quote &q)
 {
@@ -12,20 +14,41 @@ void print_debug(Quote &q)
 int main(){
     //std::vector<Quote&> vec;
     std::vector<Quote> vec;
-    Bulk_quote bq1("c",20.50,20,0.8);
-    Bulk_quote bq2("c",20.50,20,0.8);
-    Bulk_quote bq3("c",20.50,20,0.8);
+    std::vector<shared_ptr<Quote>> pvec;
 
+    Bulk_quote bq1("c",20.50,10,0.8);
+    Bulk_quote bq2("c",20.50,10,0.8);
+    Bulk_quote bq3("c",20.50,10,0.8);
     vec.push_back(bq1);
     vec.push_back(bq2);
     vec.push_back(bq3);
+
+    auto bq4 = std::make_shared<Bulk_quote>("c",20.50,10,0.8);
+    auto bq5 = std::make_shared<Bulk_quote>("c",20.50,10,0.8);
+    auto bq6 = std::make_shared<Bulk_quote>("c",20.50,10,0.8);
+    /*
+    auto bq4 = std::make_shared<Bulk_quote>(Bulk_quote("c",20.50,10,0.8));
+    auto bq5 = std::make_shared<Bulk_quote>(Bulk_quote("c",20.50,10,0.8));
+    auto bq6 = std::make_shared<Bulk_quote>(Bulk_quote("c",20.50,10,0.8));
+    */
+    pvec.push_back(bq4);
+    pvec.push_back(bq5);
+    pvec.push_back(bq6);
+
 
     auto sum = 0.0f;
     for(auto bq : vec)
     {
         sum +=bq.net_price(20);
     }
-    std::cout << "sum :" << sum << std::endl;
+    std::cout << "Quote sum :" << sum << std::endl;
+
+    sum = 0.0f;
+    for(auto pbq : pvec)
+    {
+        sum +=(*pbq).net_price(20);
+    }
+    std::cout << "Bulk_quote sum :" << sum << std::endl;
     return 0;
 
 }
